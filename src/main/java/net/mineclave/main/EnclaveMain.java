f5d0b2f194f0950f71f8f1173fd5e7c1ca5852c8
Mark Joshua Zuniga <markzuniga6@gmail.com>
2020-03-13 18:48:40 +0800

    final testing commit

--- src/main/java/net/mineclave/main/EnclaveMain.java
+++ src/main/java/net/mineclave/main/EnclaveMain.java
@@ -5,8 +5,10 @@ import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.mineclave.main.items.ClaveMaterial;
 import net.mineclave.main.items.DebugItem;
 import net.mineclave.main.items.SteelItem;
 import net.mineclave.main.items.WeaponItem;
@@ -33,17 +35,22 @@ public class EnclaveMain implements ModInitializer {
 	})
 	.build();
 
+	// ToolMaterial
+	private static final ClaveMaterial material = new ClaveMaterial(1, 0, 0.5F, 2F, 750, () -> {return Ingredient.ofItems(EnclaveMain.steel);});
+	
 	// Items
 	public static final DebugItem debug = new DebugItem(new Item.Settings().group(EnclaveMain.ENCLAVEITEMS));
-	public static final WeaponItem batonweap = new WeaponItem(2,new Item.Settings().group(EnclaveMain.ENCLAVEWEAP).maxCount(1));
+	public static final WeaponItem batonweap = new WeaponItem(material, 2, 1, new Item.Settings().group(EnclaveMain.ENCLAVEWEAP).maxCount(1).maxDamage(450));
 	public static final SteelItem steel = new SteelItem(new Item.Settings().group(EnclaveMain.ENCLAVEMATERIALS));
 
 	// Initialization
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("mineclave", "debugitem"), debug);
+
 		Registry.register(Registry.ITEM, new Identifier("mineclave", "wep_baton"), batonweap);
-		Registry.register(Registry.ITEM, new Identifier("mineclave","steelingot"), steel);
+
+		Registry.register(Registry.ITEM, new Identifier("mineclave","steel_ingot"), steel);
 		System.out.print("Item initialization complete.");
 	}
 }
--- src/main/java/net/mineclave/main/items/WeaponItem.java
+++ src/main/java/net/mineclave/main/items/WeaponItem.java
@@ -1,40 +1,30 @@
 package net.mineclave.main.items;
 
 import java.util.List;
-import com.google.common.collect.Multimap;
 
-import net.mineclave.main.EnclaveMain;
+import com.google.common.collect.Multimap;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ToolItem;
-import net.minecraft.recipe.Ingredient;
+import net.minecraft.item.SwordItem;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
-public class WeaponItem extends ToolItem {
-    private static float attackDamage;
-    private static ClaveMaterial material = new ClaveMaterial(1, 0, 5F, attackDamage, 750, () -> {return Ingredient.ofItems(EnclaveMain.steel);});
+public class WeaponItem extends SwordItem {
+    private final float attackDamage;
+    private final float attackSpeed;
 
-    public WeaponItem(final int attackDamage, final Settings settings) {
-        super(material, settings);
-        WeaponItem.attackDamage = (float) attackDamage + material.getAttackDamage();
+    public WeaponItem(ClaveMaterial material, int attackDamage, float attackSpeed, final Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+        this.attackSpeed = attackSpeed;
+        this.attackDamage = (float) attackDamage + material.getAttackDamage();
     }
 
     public float getAttackDamage() {
-        return WeaponItem.attackDamage;
-     }
-  
-    @Override
-    public TypedActionResult<ItemStack> use(final World world, final PlayerEntity playerEntity, final Hand hand) {
-        return new TypedActionResult<>(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
+        return this.attackDamage;
     }
 
     @Override
@@ -47,7 +37,8 @@ public class WeaponItem extends ToolItem {
     public Multimap<String, EntityAttributeModifier> getModifiers(final EquipmentSlot slot) {
         final Multimap<String, EntityAttributeModifier> multimap = super.getModifiers(slot);
         if (slot == EquipmentSlot.MAINHAND) {
-           multimap.put(EntityAttributes.ATTACK_DAMAGE.getId(), new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_UUID, "Weapon modifier", (double) WeaponItem.attackDamage, EntityAttributeModifier.Operation.ADDITION));
+           multimap.put(EntityAttributes.ATTACK_DAMAGE.getId(), new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_UUID, "Weapon modifier", (double)this.attackDamage, EntityAttributeModifier.Operation.ADDITION));
+           multimap.put(EntityAttributes.ATTACK_SPEED.getId(), new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_UUID, "Weapon modifier", (double)this.attackSpeed, EntityAttributeModifier.Operation.ADDITION));
         }
   
         return multimap;
