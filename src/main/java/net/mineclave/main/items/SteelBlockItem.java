a629b9f81729a45ffaed9a8aa446784a1b219d1d
Mark Joshua Zuniga <markzuniga6@gmail.com>
2020-03-14 17:20:47 +0800

    two new blocks

---
+++ src/main/java/net/mineclave/main/items/SteelBlockItem.java
@@ -0,0 +1,12 @@
+package net.mineclave.main.items;
+
+import net.minecraft.block.Block;
+
+public class SteelBlockItem extends Block
+{
+
+    public SteelBlockItem(Settings settings) {
+        super(settings);
+    }
+
+}
